a761f325fbf4975df1ca1bb3bf41d70d2ea1f4f8
Danny <eck.danny@gmail.com>
2019-06-04 22:43:32 -0700

    add more examples (#7)

--- src/modules/Colors/Colors.tsx
+++ src/modules/Colors/Colors.tsx
@@ -27,20 +27,24 @@ const Colors: React.FC = () => {
         Color
       </AnchorLink>
       <p className="lead">Let's look at simple color contrast issues...</p>
-      <AnchorLink id="Buttons" tag="h3">
-        Buttons
-      </AnchorLink>
+      <p>Probably one of every project's biggest bike-shedding topics.</p>
 
-      <p>
-        Behold the mighty <code>Button</code> component!
-      </p>
-      {Btns}
-      <p>
-        P.S. There are no exceptions for <code>:pseudo-class</code> states. If
-        you're building a library, the combinatorial explosion of independent
-        properties (size, color, disabled, psuedo-classes, ... ){' '}
-        <em>can increase</em> quickly!
-      </p>
+      <section>
+        <AnchorLink id="Buttons" tag="h3">
+          Buttons
+        </AnchorLink>
+        <p>
+          Behold the mighty <code>Button</code> component!
+        </p>
+        {Btns}
+
+        <p>
+          P.S. There are no exceptions for <code>:pseudo-class</code> states. If
+          you're building a library, the combinatorial explosion of independent
+          properties (size, color, disabled, psuedo-classes, ... ){' '}
+          <em>can increase</em> quickly!
+        </p>
+      </section>
     </div>
   )
 }
